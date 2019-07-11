d82b08ff5dcb25094dcc59a747a67ccabb3fede2
Tarun S Dudhatra <tdudhatra@gmail.com>
2019-07-11 10:23:52 +0530

    Initial Commit

---
+++ CouchDbList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace CouchDbList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string couchdbURL = "http://localhost:5984/";
+            string apiEndPoint = "_all_dbs";
+            HttpClient httpClient = new HttpClient();
+            
+            string userDbUri = couchdbURL + apiEndPoint;
+            var APIResponse = httpClient.GetAsync(userDbUri).Result.Content.ReadAsStringAsync();
+            var result = APIResponse.Result;
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
